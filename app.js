0906cd556940df18958da38d48262465918238fa
unknown <Rajankumar@addictivemedia.co.in>
2019-10-07 19:28:28 +0530

    dfasdfdasfdas

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ campgroundroutes	=require("./routes/campground"),
 indexroutes 		=require("./routes/index"),
 adminroutes			=require("./routes/admin");
 
-seedDB();
+
 
 mongoose.connect('mongodb+srv://rajankr5422:Rajan@9818@cluster0-jlil9.mongodb.net/Yelp_camp?retryWrites=true&w=majority', {useNewUrlParser: true});
 
