e6ae24b9c325d716c4c56eff82d86f7b264e9c8c
unknown <Rajankumar@addictivemedia.co.in>
2019-10-24 16:15:30 +0530

    dsfasdfsdf

--- routes/campground.js
+++ routes/campground.js
@@ -6,11 +6,12 @@ var passport       =require("passport");
 
 
 router.get("/blogs",function(req,res){
+var filter_value="All";
 	Campground.find({},function(err,allcampgrounds){
 		if(err){
 			console.log(err);
 		}else{
-			res.render("campgrounds/campgrounds",{campgrounds:allcampgrounds.reverse()});
+			res.render("campgrounds/campgrounds",{campgrounds:allcampgrounds.reverse(),filter_value:filter_value});
 
 		}
 	});
@@ -18,28 +19,73 @@ router.get("/blogs",function(req,res){
 
 
 router.post("/blogs/category/All",function(req,res){
-var filter_value=req.params.id;
-console.log(filter_value);
+var filter_value="All";
     Campground.find({}, function(err, foundcampgrounds) {
 		if(err){
 			console.log(err);
 		}else{ 
-      		  res.render("campgrounds/campgrounds",{campgrounds:foundcampgrounds});
+      		  res.render("campgrounds/campgrounds",{campgrounds:foundcampgrounds,filter_value:filter_value});
 		}
 	});
 });
 
 
-router.post("/blogs/category/:id",function(req,res){
-var filter_value=req.params.id;
-console.log(filter_value);
+router.post("/blogs/category/Health-&-fitness",function(req,res){
+var filter_value="Health & fitness";
     Campground.find({'blogtype': filter_value}, function(err, foundcampgrounds) {
 		if(err){
 			console.log(err);
 		}else{
-			console.log(foundcampgrounds);
-      		  // res.redirect("/campgrounds#row filter_listing_container",{campgrounds:allcampgrounds.reverse()});  
-      		  res.render("campgrounds/campgrounds",{campgrounds:foundcampgrounds});
+      		  res.render("campgrounds/campgrounds",{campgrounds:foundcampgrounds,filter_value:filter_value});
+		}
+	});
+});
+
+router.post("/blogs/category/Lifestyle-&-Leisure",function(req,res){
+var filter_value="Lifestyle & Leisure";
+    Campground.find({'blogtype': filter_value}, function(err, foundcampgrounds) {
+		if(err){
+			console.log(err);
+		}else{
+      		  res.render("campgrounds/campgrounds",{campgrounds:foundcampgrounds,filter_value:filter_value});
+		}
+	});
+});
+
+
+router.post("/blogs/category/Design-&-Art",function(req,res){
+var filter_value="Design & Art";
+    Campground.find({'blogtype': filter_value}, function(err, foundcampgrounds) {
+		if(err){
+			console.log(err);
+		}else{
+      		  res.render("campgrounds/campgrounds",{campgrounds:foundcampgrounds,filter_value:filter_value});
+		}
+	});
+});
+
+
+
+router.post("/blogs/category/Self-Development",function(req,res){
+var filter_value="Self Development";
+    Campground.find({'blogtype': filter_value}, function(err, foundcampgrounds) {
+		if(err){
+			console.log(err);
+		}else{
+      		  res.render("campgrounds/campgrounds",{campgrounds:foundcampgrounds,filter_value:filter_value});
+		}
+	});
+});
+
+
+
+router.post("/blogs/category/:id",function(req,res){
+var filter_value=req.params.id;
+    Campground.find({'blogtype': filter_value}, function(err, foundcampgrounds) {
+		if(err){
+			console.log(err);
+		}else{
+      		  res.render("campgrounds/campgrounds",{campgrounds:foundcampgrounds,filter_value:filter_value});
 		}
 	});
 });
